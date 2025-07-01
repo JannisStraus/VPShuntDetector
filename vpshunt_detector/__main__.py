49e6c5d05c919c9bb3721e75bb92598221ed1efd
Jannis Straus <jannis.straus@uk-essen.de>
2025-07-01 08:18:50 +0200

    improve code

--- vpshunt_detector/__main__.py
+++ vpshunt_detector/__main__.py
@@ -1,18 +1,40 @@
 import argparse
+from pathlib import Path
 
 from vpshunt_detector.inference import infer
+
+
+def _existing(path_str: str) -> Path:
+    path = Path(path_str).expanduser().resolve()
+    if not path.exists():
+        raise argparse.ArgumentTypeError(f"'{path}' is not an existing directory")
+    return path
+
+
+def _existing_dir(path_str: str) -> Path:
+    path = Path(path_str).expanduser().resolve()
+    if not path.is_dir():
+        raise argparse.ArgumentTypeError(f"'{path}' is not an existing directory")
+    return path
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(
         description="Detect VPShunt valves in X-ray images."
     )
-    parser.add_argument("-i", "--input", required=True, help="Path to input images.")
     parser.add_argument(
-        "-o", "--output", required=True, help="Path to save detection results."
+        "-i", "--input", type=_existing, required=True, help="Path to input images."
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        required=True,
+        help="Path to save detection results.",
     )
     parser.add_argument(
         "--instructions",
+        type=_existing_dir,
         required=False,
         help="Directory with instruction images for valve models.",
     )
@@ -24,6 +46,7 @@ def main() -> None:
     )
     args = parser.parse_args()
 
+    args.output.mkdir(parents=True, exists_ok=True)
     infer(
         args.input,
         args.output,
--- vpshunt_detector/utils.py
+++ vpshunt_detector/utils.py
@@ -1,27 +1,75 @@
-import os
-import sys
+import logging
 from pathlib import Path
+from typing import Any
+
+import cv2
+from cv2.typing import MatLike
+
+BBox = tuple[int, int, int, int] | None
+logger = logging.getLogger(__name__)
 
 
 def get_cache_dir() -> Path:
-    if sys.platform == "win32":
-        # Use the LOCALAPPDATA environment variable on Windows.
-        local_appdata = os.getenv("LOCALAPPDATA")
-        if local_appdata:
-            cache_dir = Path(local_appdata) / ".cache"
-        else:
-            cache_dir = Path.home() / ".cache"
-    elif sys.platform == "darwin":
-        # Use the macOS default cache directory.
-        cache_dir = Path.home() / "Library" / "Caches"
-    else:
-        # On Linux/Unix, check XDG_CACHE_HOME; fallback to ~/.cache.
-        xdg_cache = os.getenv("XDG_CACHE_HOME")
-        if xdg_cache:
-            cache_dir = Path(xdg_cache)
-        else:
-            cache_dir = Path.home() / ".cache"
-
-    # Create the cache directory if it does not exist.
+    cache_dir = Path.home() / ".cache"
     cache_dir.mkdir(parents=True, exist_ok=True)
     return cache_dir
+
+
+def save_bbox(
+    image_path: str | Path,
+    output_path: str | Path,
+    bbox: BBox,
+    cls: str,
+    **kwargs: Any,
+) -> None:
+    instruction_dir: str | Path | None = kwargs.get("instruction_dir", None)
+    missing_instructions: set[str] | None = kwargs.get("missing_instructions", None)
+    img = draw_bbox(image_path, bbox, cls, instruction_dir, missing_instructions)
+    cv2.imwrite(str(output_path), img)
+
+
+def draw_bbox(
+    image_path: str | Path,
+    bbox: BBox,
+    cls: str,
+    instruction_dir: str | Path | None = None,
+    missing_instructions: set[str] | None = None,
+) -> MatLike:
+    img = cv2.imread(str(image_path))
+    if not bbox:
+        return img
+    if missing_instructions is None:
+        missing_instructions = set()
+    p1, p2 = bbox[:2], bbox[2:]
+    img = cv2.rectangle(img, p1, p2, color=(0, 255, 0), thickness=4)
+    img = cv2.putText(
+        img,
+        cls,
+        (p1[0], max(0, p1[1] - 10)),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        thickness=4,
+    )
+    if not instruction_dir:
+        return img
+    instruction_dir = Path(instruction_dir)
+    manufacturer_path = next(instruction_dir.glob(f"{cls}.*"), None)
+    if manufacturer_path:
+        manufacturer_img = cv2.imread(str(manufacturer_path))
+        manufacturer_img = cv2.resize(
+            manufacturer_img,
+            (
+                int(
+                    manufacturer_img.shape[1] * img.shape[0] / manufacturer_img.shape[0]
+                ),
+                img.shape[0],
+            ),
+        )
+        img = cv2.hconcat([img, manufacturer_img])
+    elif cls not in missing_instructions:
+        missing_instructions.add(cls)
+        logger.warning(
+            f"Manufacturer image '{cls}.png' or '{cls}.jpg' is missing in '{instruction_dir}'."
+        )
+    return img
